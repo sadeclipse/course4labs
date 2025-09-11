8a6092185b02f832a99d2925759e59c62f2f882b
sadeclipse <139959215+sadeclipse@users.noreply.github.com>
2025-09-11 13:29:09 +0300

    Updated lab

--- mpt/1lab/main.c
+++ mpt/1lab/main.c
@@ -1,16 +1,17 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
+#include <math.h>
 
 #define EMPTY 10
 #define MINUS 11
 
 void clearSegments(void);
-void numout (uint32_t number);
-void drawposneg(uint32_t number);
-uint8_t Digit (uint32_t d, uint8_t m);
+void numout(int32_t number);
+void drawposneg(int32_t number);
+int8_t Digit(int32_t d, uint8_t m);
 
-uint16_t angle = 0;
+int32_t angle = 100;
 const uint8_t segments[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F, 0, 0x40}; // добавить функцию digit и нормально выводить число angle +добавить ограничения и минусы
 
 int main(void)
@@ -22,66 +23,76 @@ int main(void)
     sei();
     while (1)
     {
-        numout()
+        numout(angle);
     }
 }
 
-uint8_t Digit (uint32_t d, uint8_t m)
-{  
-    uint8_t i = 4, a;  
-    while(i) 
-    {   // цикл по разрядам числа 
-        a = d%10;   // выделяем очередной разряд 
-        if(i-- == m) break;  // выделен заданный разряд - уходим 
-        d /= 10;   // уменьшаем число в 10 раз 
-    } 
-    return(a); 
+int8_t Digit(int32_t d, uint8_t m)
+{
+
+    int8_t i = 5, a;
+    if (d < 0)
+        d *= -1;
+    while (i)
+    {               // цикл по разрядам числа
+        a = d % 10; // выделяем очередной разряд
+        if (i-- == m)
+            break; // выделен заданный разряд - уходим
+        d /= 10;   // уменьшаем число в 10 раз
+    }
+    return (a);
 }
 
-void numout (uint32_t number)           // функция, которая выводит цифру
+void numout(int32_t number) // функция, которая выводит цифру
 {
-    if ((number < 9999) & (number > -9999))
+
+    if (number == 0)
     {
-        uint8_t i = 1;                                   // номер разряда
-        while(Digit(number,i) == 0)                      // с помощью этого цикла мы убираем нули
-        {
-            PORTA = (1<<i);
-            PORTC = 0x0;  
-            _delay_us(5);                                    // пауза
-            PORTA &= ~ (1 << i);
-            i++;                                            // переход в следующий разряд
-        }
+        PORTA |= (1 << 5);   // подача в регистр нужной последовательности, чтобы выбрать нужный разряд
+        PORTC = segments[0]; // подача изображения нужной цифры
+        _delay_us(10);
+        PORTA &= ~(1 << 5);
+        return;
+    }
 
-        _delay_ms(50);
+    uint8_t i = 2; // номер разряда
 
-        while (i <= 4)                                      //запоминает i при котором у нас первая не нулевая цифра и выводит последовательно оставшиеся цифры в разряды
-        {
-            PORTA = (1 << i);                               // подача в регистр нужной последовательности, чтобы выбрать нужный разряд
-            _delay_us(5);
-            PORTC = segments[Digit(number,i)];              // подача изображения нужной цифры
-            PORTA &= ~ (1 << i);                            // ставим регистр в изначальное значение
-            i++;                                            // переход в следующий разряд
-        }
+    while (Digit(number, i) == 0) // с помощью этого цикла мы убираем нули
+    {
+        PORTA |= (1 << i);
+        PORTC = 0x0;
+        _delay_us(20); // пауза
+        PORTA &= ~(1 << i);
+        i++; // переход в следующий разряд
+    }
+
+    while (i <= 5) // запоминает i при котором у нас первая не нулевая цифра и выводит последовательно оставшиеся цифры в разряды
+    {
+        PORTA |= (1 << i);
+         // подача в регистр нужной последовательности, чтобы выбрать нужный разряд
+        PORTC = segments[Digit(number, i)];
+        _delay_us(20);      // подача в регистр нужной последовательности, чтобы выбрать нужный разряд
+        PORTA &= ~(1 << i); // ставим регистр в изначальное значение
+        i++;                // переход в следующий разряд
     }
     drawposneg(number);
-
 }
 
-void drawposneg(uint32_t number)
+void drawposneg(int32_t number)
 {
     if (number < 0)
     {
-        PORTA = (1 << 5);                               // подача в регистр нужной последовательности, чтобы выбрать нужный разряд
-        _delay_us(5);
-        PORTC = segments[MINUS];              // подача изображения нужной цифры
-        PORTA &= ~ (1 << 5);  
+        PORTA |= (1 << 1);       // подача в регистр нужной последовательности, чтобы выбрать нужный разряд
+        PORTC = segments[MINUS]; // подача изображения нужной цифры
+        _delay_us(10);
+        PORTA &= ~(1 << 1);
     }
     else
     {
-        PORTA = (1 << 5);                               // подача в регистр нужной последовательности, чтобы выбрать нужный разряд
-        _delay_us(5);
-        PORTC = segments[EMPTY];              // подача изображения нужной цифры
-        PORTA &= ~ (1 << 5);  
+        PORTA |= (1 << 1); // подача в регистр нужной последовательности, чтобы выбрать нужный разряд
+        PORTC = segments[EMPTY];
+        _delay_us(10);
+        PORTA &= ~(1 << 1);
     }
 }
 
