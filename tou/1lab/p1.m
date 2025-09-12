7607aa9dc7c497be5c24d3e40b7b74f5d3573a11
sadeclipse <nedoviziy2004@gmail.com>
2025-09-12 23:57:33 +0300

    Done 1st lab

--- tou/1lab/p1.m
+++ tou/1lab/p1.m
@@ -1,35 +1,48 @@
 n = 1; %% номер варианта
 %a
-a  = randi(44, 5, 1);
-b = randi(44, 5, 1);
+disp('Пункт a')
+a  = randi(44, 5, 1)
+b = randi(44, 5, 1)
 %b
-sum = a + b;
+disp('Пункт b')
+sum = a + b
 %c
-prod = a* (b.');
+disp('Пункт c')
+prod = a* (b.')
 %d
-A = randi(88, 3, 4);
+disp('Пункт d')
+A = randi(88, 3, 4)
 %e
-second_row = A(2,:);
+disp('Пункт e')
+second_row = A(2,:)
 %f
-third_col = A(:, 3);
+disp('Пункт f')
+third_col = A(:, 3)
 %g
-C = randi(4, 4, 4);
+disp('Пункт g')
+C = randi(4, 4, 4)
 %h
-c_det = det(C);
+disp('Пункт h')
+c_det = det(C)
 %i
-eig_c = eig(C);
+disp('Пункт i')
+eig_c = eig(C)
 %j
-max_c = max(max(C));
+disp('Пункт j')
+max_c = max(max(C))
 %k
-max_second_col = max(C);
-max_second_col = max_second_col(2);
+disp('Пункт k')
+max_second_col = max(C)
+max_second_col = max_second_col(2)
 %l
-s = [1 + n, 7, 8 - n];
-coefs = poly(s);
+disp('Пункт l')
+s = [1 + n, 7, 8 - n]
+coefs = poly(s)
 %m
-sin_deg = sin(deg2rad(21 + n));
-cos_deg = cos(deg2rad(21 + n));
-tan_deg = tan(deg2rad(21 + n));
-
+disp('Пункт m')
+sin_deg = sin(deg2rad(21 + n))
+cos_deg = cos(deg2rad(21 + n))
+tan_deg = tan(deg2rad(21 + n))
 %n
+disp('Пункт n')
 reminder = mod((38*n), (6 + n)) 
